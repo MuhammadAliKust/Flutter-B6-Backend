78736246f4a2a5f0dfb40b82ac79d5051c814fe8
Muhammad Ali Nizami <kustalics@gmail.com>
2024-11-05 11:14:36 +0500

    Task Services Done

--- lib/services/task.dart
+++ lib/services/task.dart
@@ -29,8 +29,43 @@ class TaskServices {
   }
 
   ///Get All Tasks
+  Stream<List<TaskModel>> getAllTasks() {
+    return FirebaseFirestore.instance
+        .collection('taskCollection')
+        .snapshots()
+        .map((taskList) {
+      return taskList.docs
+          .map((taskModel) => TaskModel.fromJson(taskModel.data()))
+          .toList();
+    });
+  }
+
   ///Get Completed Tasks
+  Stream<List<TaskModel>> getCompletedTasks() {
+    return FirebaseFirestore.instance
+        .collection('taskCollection')
+        .where('isCompleted', isEqualTo: true)
+        .snapshots()
+        .map((taskList) {
+      return taskList.docs
+          .map((taskModel) => TaskModel.fromJson(taskModel.data()))
+          .toList();
+    });
+  }
+
   ///Get InCompleted Tasks
+  Stream<List<TaskModel>> getInCompletedTasks() {
+    return FirebaseFirestore.instance
+        .collection('taskCollection')
+        .where('isCompleted', isEqualTo: false)
+        .snapshots()
+        .map((taskList) {
+      return taskList.docs
+          .map((taskModel) => TaskModel.fromJson(taskModel.data()))
+          .toList();
+    });
+  }
+
   ///Mark task as complete
   Future markTaskAsComplete(String taskID) async {
     await FirebaseFirestore.instance
